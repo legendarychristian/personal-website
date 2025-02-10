f6a01d19992164f21fb1643e95d5e85a21bb831a
legendarychristian <christian.jairolopez@gmail.com>
2025-02-10 13:06:45 -0500

    need to put social media logos

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -7,27 +7,27 @@ export default function AboutSection() {
                     About Me
                 </h1>
                 <div className="flex flex-col font-openSans pt-12 px-36">
-                    <p className="mt-4 text-xl text-[#B2B5DA] font-light leading-relaxed">
+                    <p className="mt-4 text-3xl text-[#B2B5DA] font-light leading-relaxed">
                         I am a passionate scientist with expertise in developing scalable AI systems, dynamic web applications,
                         and advanced data processing solutions.
                     </p>
-                    <p className="mt-4 text-xl text-[#B2B5DA] font-light leading-relaxed">
+                    <p className="mt-4 text-3xl text-[#B2B5DA] font-light leading-relaxed">
                         My work focuses on utilizing AWS services, GPU programming, and fine-tuning foundation models
                         to tackle complex challenges across various domains.
                     </p>
-                    <p className="mt-4 text-xl text-[#B2B5DA] font-light leading-relaxed">
+                    <p className="mt-4 text-3xl text-[#B2B5DA] font-light leading-relaxed">
                         I enjoy tackling challenging problems that require me to piece together different technologies in unique ways.
                     </p>
-                    <p className="mt-4 text-xl text-[#B2B5DA] font-light leading-relaxed">
+                    <p className="mt-4 text-3xl text-[#B2B5DA] font-light leading-relaxed">
                         My passion for programming stems from a love of technology, and while programming is my specialty,
                         I am constantly seeking knowledge and learning new things about the tech industry simply because I enjoy it.
                     </p>
-                    <p className="mt-4 text-xl text-[#B2B5DA] font-light leading-relaxed">
+                    {/* <p className="mt-4 text-3xl text-[#B2B5DA] font-light leading-relaxed">
                         If you'd like to learn more about me, feel free to reach out to me on any of the following platforms:
                     </p>
                     <p className="mt-4 text-xl text-[#B2B5DA] font-light leading-relaxed">
                         I look forward to hearing from you!
-                    </p>
+                    </p> */}
                 </div>
             </div>
         </section>
